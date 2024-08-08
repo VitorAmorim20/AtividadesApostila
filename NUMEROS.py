10bf6945a42ecf9cea22828f0091a2427ef12149
VitorAmorim <asalersonvitor20@gmail.com>
2024-08-08 17:43:22 -0300

    ATIVIDADES NUM. E VARIAVEIS

---
+++ NUMEROS.py
@@ -0,0 +1,12 @@
+# x = int(input("digite um valor: "))
+# y = int(input("Digite mais um valor "))
+#
+# z = (x**2 + y**2) / (x - y)**2
+#
+# print(f"O resultado é igual a: {z}")
+
+salario = float(input("Digite o vosso salário a seguir:"))
+
+ajuste = float((salario*35)/100)
+final = salario+ajuste
+print(f"O seu salário sofrerá um ajude de {ajuste}, portanto seu salário final será de {final}")
---
+++ STRING.py
@@ -0,0 +1,12 @@
+# frase = "Um elefante incomoda muita gente"
+#
+# print(frase[3:20])
+
+
+
+frase = input("Digite uma frase: ")
+
+x = frase.strip()
+y = x.upper()
+
+print(y)
